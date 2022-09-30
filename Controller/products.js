7f396610bdfc0aa22aba145ef5e3caf613faffeb
GautamRamani <101443138+GautamRamani@users.noreply.github.com>
2022-09-30 13:12:23 +0530

    commit

--- Controller/products.js
+++ Controller/products.js
@@ -106,7 +106,7 @@ router.post("/",upload.single("image"),async(req,res)=>{
         description:req.body.description,
         richDescription:req.body.richDescription,
         image:`${basePath}${fileName}`,
-        images:req.body.images,
+        images:`${[basePath]}${[fileName]}`,
         brand:req.body.brand,
         price:req.body.price,
         category : req.body.category,
