2b3444d6b21970b2415ec7c0c5cbe544ada00178
Gautam Ramani <gautam.ramani@artoon.in>
2023-12-21 16:51:58 +0530

    add changes

--- Routes/index.js
+++ Routes/index.js
@@ -1,10 +1,13 @@
 const express=require("express")
 const router=express.Router();
 
+router.get("/test",(req,res)=>{
+    res.status(200).send({status:"success",msg:"Health work"})
+})
+
 router.use("/products",require("../Controller/products"))
 router.use("/categories",require("../Controller/categories"))
 router.use("/users",require("../Controller/users"))
 router.use("/orders",require("../Controller/orders"))
-
 
 module.exports=router;
